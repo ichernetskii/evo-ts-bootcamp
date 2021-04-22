8d768e785d20095588f9a1136dae524e38c813e3
Ilya Chernetskiy <info@smarto.pro>
2021-04-22 13:19:42 +0300

    bubble sort fix

--- homeworks/01-bubble-sort/src/assets/utils.ts
+++ homeworks/01-bubble-sort/src/assets/utils.ts
@@ -6,7 +6,5 @@ return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function randomArray(length: number = 10, from: number = 0, to: number = 100):number[] {
-return Array(length)
-    .fill(null)
-    .map(() => randomIntFromInterval(from, to));
+    return Array.from({length}, () => randomIntFromInterval(from, to));
 }
