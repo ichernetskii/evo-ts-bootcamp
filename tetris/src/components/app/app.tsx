19e35a13f1a7dacaf356f20fd4e5df26f409107d
Ilya Chernetskiy <info@smarto.pro>
2021-06-20 03:09:23 +0300

    add react, ts, webpack, babel

---
+++ tetris/src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import s from  "./app.module.scss";
+
+const App: React.FC<{}> = () => {
+    return (
+        <div className={s.app}>
+            Application
+            asd
+        </div>
+    );
+};
+
+export default App;
